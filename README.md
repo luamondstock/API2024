ac5303fa461de29a718fc04ff5080441679c4c03
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-10-10 12:16:39 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# tratamento.de.dados
+https://script.google.com/a/macros/escola.pr.gov.br/s/AKfycbwM4kmJt7BbmIiud69wxaXRmXfOimEiYvmSc3y4j8_ZDOqYTrUaTrUuiMyJDi5wmPzY/exec
---
+++ graficos/graficoPizza.js
@@ -0,0 +1,36 @@
+import { pegarCSS, configuraEixo} from "./comum.js"
+
+async function criarGraficoPizza(){
+    const url = 'https://raw.githubusercontent.com/luamondstock/API2024/refs/heads/main/entrevista.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const livros = Object.keys(dados)
+    const valores = Object.values(dados)
+
+    const data = [ 
+      {
+        values: valores,
+        labels: livros,
+        type: 'pie', 
+        textinfo: 'label + percent', 
+      }
+    ]
+    const layout = {
+        height: 400, 
+        width: 600, 
+        plot_bgcolor: pegarCSS('--cor02'),
+        paper_bgcolor: pegar('--cor03')
+    }
+    const livrosTitulo = document.createElement("h3")
+    livrosTitulo.classList.add("caixa-grafico__titulo")
+    livrosTitulo.innerHTML = ' Os <span>livros</span> mais vendidos da editora seguinte em 2023'
+
+      const grafico = document.createElement('div')
+    grafico.className = 'grafico-disco'
+    document.getElementById('caixa-grafico').appendChild(livrosTitulo)
+    document.getElementById('caixa-grafico').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+    
+}
+
+criarGraficoPizza()
